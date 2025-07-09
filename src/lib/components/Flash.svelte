792c36337109ba59946efcbdec7616626db7076a
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-07-09 13:17:00 +0200

    Simplifies displaying flash

--- src/lib/components/Flash.svelte
+++ src/lib/components/Flash.svelte
@@ -1,24 +1,18 @@
 <script lang="ts">
-  import { page } from '$app/state';
   import { getFlash } from 'sveltekit-flash-message';
+  import { page } from '$app/state';
 
   const flash = getFlash(page);
-
-  let type = $state('');
-  let message = $state('');
-
-  flash.subscribe((value) => {
-    type = value ? value.type : '';
-    message = value ? value.message : '';
-  });
 </script>
 
-{#if (type == 'success')}
-  <div class="mb-8 bg-green-100 text-green-800 px-6 sm:px-8 py-4 sm:py-5">
-    {@html message}
-  </div>
-{:else if type == 'error'}
-  <div class="mb-8 bg-red-100 text-red-800 px-6 sm:px-8 py-4 sm:py-5">
-    {@html message}
-  </div>
+{#if $flash}
+  {#if ($flash.type == 'success')}
+    <div class="mb-8 bg-green-100 text-green-800 px-6 sm:px-8 py-4 sm:py-5">
+      {@html $flash.message}
+    </div>
+  {:else if $flash.type == 'error'}
+    <div class="mb-8 bg-red-100 text-red-800 px-6 sm:px-8 py-4 sm:py-5">
+      {@html $flash.message}
+    </div>
+  {/if}
 {/if}
