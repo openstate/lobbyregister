a1a066c021ac7d65c9e8ab87e6e128a2d2ba4717
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-07-09 11:02:42 +0200

    Adds flash message when logging out

--- src/routes/uitloggen/+server.ts
+++ src/routes/uitloggen/+server.ts
@@ -1,8 +1,10 @@
-import { redirect } from '@sveltejs/kit';
+import { redirect } from 'sveltekit-flash-message/server'
 import type { RequestHandler } from './$types';
 import { logoutUser } from '../../utils/authenticationUtils';
 
 export const POST: RequestHandler = ( event ) => {
     logoutUser(event.cookies);    
-    redirect(303, '/');
+
+    const message = "U bent nu uitgelogd";
+    redirect(303, '/', {type: 'success', message: message}, event.cookies);
 }
