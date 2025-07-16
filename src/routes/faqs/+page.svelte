293e9dd69b64c267bb11b2a396a46063703a8d3f
coretteket <33397397+Coretteket@users.noreply.github.com>
2025-07-16 12:13:56 +0200

    Update FAQ for Amsterdam

--- src/routes/faqs/+page.svelte
+++ src/routes/faqs/+page.svelte
@@ -13,23 +13,22 @@ van een actie gericht op beïnvloeding van openbare besluitvorming.</p>
 <p class="mb-6 text-gray-700 text-lg">Elke natuurlijke of rechtspersoon die lobbyt.</p>
 
 <h2 class="text-xl font-bold text-gray-800 mb-1">Wat is openbare besluitvorming?</h2>
-<p class="mb-6 text-gray-700 text-lg">Besluiten door wetgevende of uitvoerende macht op nationaal niveau. In een
-later stadium kunnen regionaal of lokaal niveau worden toegevoegd.</p>
+<p class="mb-6 text-gray-700 text-lg">Besluiten door het College van Burgemeester en Wethouders, de gemeenteraad en
+het Gemeentelijk Management Team van de gemeente Amsterdam.</p>
 
 <h2 class="text-xl font-bold text-gray-800 mb-1">Wie moeten zich registreren?</h2>
 <p class="mb-6 text-gray-700 text-lg">Alle externe partijen (bedrijven, PA-bureaus, maatschappelijke organisaties,
-onderwijsinstellingen, vakbonden, buurtcomités, e.d.), inclusief
-koepelorganisaties (VNG, IPO, e.d.), die willen lobbyen. Niet intern overheid, zie
-daarvoor Register van Overheidsorganisaties op overheid.nl.</p>
+onderwijsinstellingen, vakbonden, buurtcomités, e.d.) die willen lobbyen bij de
+gemeente Amsterdam. Niet van toepassing op interne gemeentelijke organisaties.</p>
 
 <h2 class="text-xl font-bold text-gray-800 mb-1">Welke afspraken staan in het lobbyregister?</h2>
 <p class="mb-6 text-gray-700 text-lg">Alle vormen van communicatie: fysiek, bellen, video, inclusief werkbezoeken,
 ontvangsten en vergelijkbare externe activiteiten. Niet: speeches, presentaties
-door de overheidsvertegenwoordiger, boekuitreikingen, e.d. en niet de contacten
+door de gemeentefunctionaris, boekuitreikingen, e.d. en niet de contacten
 die direct voortvloeien uit de functie (benoemingen, uitreiking lintje, e.d.).</p>
 
 <h2 class="text-xl font-bold text-gray-800 mb-1">Ik heb mij geregistreerd. En nu?</h2>
 <p class="mb-6 text-gray-700 text-lg">Nadat een lobbyist zich heeft geregistreerd in het lobbyregister kan de afspraak
-met de overheidsvertegenwoordiger of diens secretariaat worden gemaakt. De
-specifieke afspraak of activiteit wordt door de overheidsvertegenwoordiger of
+met de gemeentefunctionaris of diens secretariaat worden gemaakt. De
+specifieke afspraak of activiteit wordt door de betrokken functionaris of
 diens secretariaat in het lobbyregister vastgelegd en gekoppeld aan de lobbyist.</p>
