85d2e5722be8c4e46ef92bde30dd8dd2a6a99197
coretteket <33397397+Coretteket@users.noreply.github.com>
2026-02-04 12:03:06 +0100

    Checks basic auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,23 @@
 import type { Handle } from '@sveltejs/kit';
 import { getAuthenticatedUser } from './utils/authenticationUtils';
+import { getRequestEvent } from '$app/server';
+import { env } from '$env/dynamic/private';
+
+function checkBasicAuth() {
+  const authorization = getRequestEvent().request.headers.get('authorization');
+  if (!authorization || !authorization.startsWith('Basic ')) return false;
+  const credentials = Buffer.from(authorization.slice(6), 'base64').toString('utf-8');
+  return credentials === `${env.BASIC_AUTH_USERNAME}:${env.BASIC_AUTH_PASSWORD}`;
+}
 
 export const handle: Handle = async ({ event, resolve }) => {
+    // Gatekeeping authentication for the full demo
+  if (!checkBasicAuth())
+    return new Response('Unauthorized', {
+      headers: { 'WWW-Authenticate': 'Basic realm="lobbyregister"' },
+      status: 401,
+    });
+
   event.locals.user = await getAuthenticatedUser(event.cookies);
 
   const response = await resolve(event);
